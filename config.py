faf03eb7c8adbff9d44518c66ed6269108a71b22
raphael08 <rsiphael@gmail.com>
2023-07-01 14:49:38 +0300

    .

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:raphael@localhost:5432/crime'
 #SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/crime'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:XSfmREXCi1qjdIqNxeOp@containers-us-west-167.railway.app:6146/railway'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:F3Ll2nrtB1bxTaPUw8FS@containers-us-west-19.railway.app:7351/railway'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 #SECURITY_PASSWORD_HASH = 'bcrypt'
